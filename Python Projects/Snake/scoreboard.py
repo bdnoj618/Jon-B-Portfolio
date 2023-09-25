96672626ccdb133ee291b1170cc0646bdcdc05b6
bdnoj618 <51130420+bdnoj618@users.noreply.github.com>
2023-09-25 12:54:41 -0500

    Update scoreboard.py
    Added a highscore tracker for when game is reran

--- Python Projects/Snake/scoreboard.py
+++ Python Projects/Snake/scoreboard.py
@@ -6,14 +6,20 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.color("White")
         self.penup()
         self.setpos(0,265)
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
     def refresh(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0,0)
-        self.write("GAME OVER",align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("data.txt", mode="w") as data:
+                data.write(f"{self.high_score}")
+        self.score = 0
+        self.refresh()
